16fc5d434ebf325c0a839110b954f5a8af517e25
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-08-15 12:53:27 +0200

    Se actualizo el archivo Logo.jsx

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Logo = () => {
   return (
     <Link href="/">
-      <div className="flex flex-col justify-center items-start uppercase leading-3 text-white">
+      <div className="flex flex-col justify-center items-start uppercase text-white">
         <p className="font-semibold lg:text-xl md:text-xl sm:text-base font-serif">one</p>
         <p className="font-semibold font-serif lg:text-base md:text-base sm:text-sm">second</p>
       </div>
